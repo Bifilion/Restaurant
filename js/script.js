6385d9ea59211055352861c9001063d5ca11df68
Jiri <jiriplic@seznam.cz>
2020-11-05 12:41:01 +0100

    create contact, footer and navbar, smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,50 @@
 $(document).ready(function() {
+
+    $(".navbar-toggler").click(function() {
+        $(".navbar-toggler").toggleClass("change");
+    });
+
+    //sticky navbar
+
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+
+        if (position >= 718) {
+            $(".navbar").addClass("navbar-background");
+            $(".navbar").addClass("fixed-top");
+        } else {
+            $(".navbar").removeClass("navbar-background");
+            $(".navbar").removeClass("fixed-top");
+        }
+    });
+
+    //smooth scroll
+
+    $(".nav-item a, .header-link, #back-to-top").click(function(link) {
+        link.preventDefault();
+
+        let target = $(this).attr("href");
+
+        $("html, body").stop().animate({
+            scrollTop: $(target).offset().top - 25
+        }, 3000);
+    });
+
+    //back to top
+
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+
+        if (position >= 718) {
+            $("#back-to-top").addClass("scrollTop");
+
+        } else {
+            $("#back-to-top").removeClass("scrollTop");
+        }
+    });
+
+
+
     //ripples
     $("#header,.info").ripples({
         dropRadius: 20,
@@ -15,20 +61,6 @@ $(document).ready(function() {
             enabled: true
         }
     });
-
-
-
-    /* $('.navbar-toggler').click(function() {
-        $('.navbar-toggler').toggleClass('change');
-
-    })
-*/
-    //window scroll
-
-
-    //smooth scroll
-
-
 
 
 });
